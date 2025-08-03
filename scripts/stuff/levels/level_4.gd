9d15009c072d7cab8b110caf61cbfc066d3a2413
KirboTheDirbo <kirbotdirbo@gmail.com>
2025-08-03 21:19:45 +1000

    proper final jam release

--- scripts/stuff/levels/level_4.gd
+++ scripts/stuff/levels/level_4.gd
@@ -24,3 +24,7 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	Global.springSpawn = Vector2.ZERO
 	get_tree().change_scene_to_file("res://scenes/levels/level5.tscn")
+
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
